75a000c118ea2fca4c183b1479b92d036765ca1a
thisiskarthisk <karthikeyan2771999@gmail.com>
2024-07-12 11:33:03 +0530

    Updates

---
+++ precache-manifest.9aee664138d66d293d0f7ec663dfa9f0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9981189c912d4bdb78ae777a58ee7184",
+    "url": "/index.html/index.html"
+  },
+  {
+    "revision": "88654c0e69954e323a99",
+    "url": "/index.html/static/css/main.435d5fe5.chunk.css"
+  },
+  {
+    "revision": "aea54c2cc0e9daa36ce6",
+    "url": "/index.html/static/js/2.bbc244fd.chunk.js"
+  },
+  {
+    "revision": "ea935f47a7b92aa14dc7",
+    "url": "/index.html/static/js/3.a29efab2.chunk.js"
+  },
+  {
+    "revision": "88654c0e69954e323a99",
+    "url": "/index.html/static/js/main.26948cee.chunk.js"
+  },
+  {
+    "revision": "20b264e4fce3e0157932",
+    "url": "/index.html/static/js/runtime~main.81d63b2f.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/index.html/static/media/logo.06e73328.svg"
+  }
+]);
